53bd6b232bee1725936b724270620b34415cc9f8
Frankieeee <frankokiya@gmail.com>
2017-09-19 23:07:25 +0800

    Updated GroceryList

--- GroceryItem.java
+++ GroceryItem.java
@@ -12,12 +12,25 @@ class GroceryItem {
     this.quantity += quantity;
   }
   
+  void reduce(int quantity) {
+    this.quantity -= quantity;
+  }
+  
+  
   public String toString() {
     if (isBought) {
       return "[x] " + this.name + " " + this.quantity; 
     } else {
       return "[ ] " + this.name + " " + this.quantity;
     }
+  }
+
+  public void buy() {
+    this.isBought = true;
+  }
+  
+  public int getQuantity() {
+    return this.quantity;
   }
   
   public boolean equals(Object o) {
--- GroceryListArray.java
+++ GroceryListArray.java
@@ -8,54 +8,59 @@ class GroceryListArray implements IGroceryList {
     
   
   public boolean add(GroceryItem item) {
-    if (size == max) {
+    
+    int i = indexOf(item);
+    if (i > -1) {
+      items[i].addQuantity(item.quantity);
+      return true;
+    } else if (size == max) {
       // create new array with bigger length
       // int max = max + 10;
       // GroceryItems bigger[] = new GroceryItem[max]
       // copy items to new array
       // assign new array to items
       
-      return false; // don't return instead
-    }
-    
-    int i = indexOf(item.name);
-    if (i > -1) {
-      items[i].addQuantity(item.quantity);
-    } else {    
-      items[this.size] = item;
-      this.size++;
       
-    }
+      /* Fields:
+       * max -- int
+       * items[] -- GroceryItem
+       * size -- int
+       * 
+       * Methods:
+       * indexOf(GroceryItem)
+       * remove(String)
+       * markAsBought(String)
+       * 
+       * Fields of GroceryItem:
+       * name -- String
+       * quantity -- int
+       * isBought -- boolean
+       * 
+       * Methods of GroceryItem:
+       * addQuantity(int)
+       * buy()
+       * toString()
+       * equals(Object)
+       * 
+       */
+      //items = createNewArrayList();
+      int newMax = max + 10;
+      GroceryItem[] bigger = new GroceryItem[newMax];
+      for(int a = 0; a < this.size; a++){
+        bigger[a] = items[a];
+      }
+      items = bigger;
+      
+    }  
+    items[this.size] = item;
+    this.size++;
+    
     return true;
   }
   
-  // TEMPLATE
-  /*  Fields:
-   *    this.max
-   *    this.items   --GroceryItem[]
-   *    this.size
-   * 
-   *  Methods:
-   *    this.add
-   *    this.indexOf
-   *    this.remove
-   *    this.markAsBought
-   *    this.display
-   * 
-   *  Methods on this.items[i]:
-   *    this.items[i].addQuantity()
-   *    this.items[i].equals()
-   *    this.items[i].toString()
-   */
-  
-   
-  // String -> int
-  // Given the name of a GroceryItem, returns the
-  // corresponding GroceryItem index from the list. If it is not in the list,
-  //  returns -1
-  int indexOf(String name) {
+  int indexOf(GroceryItem item) {
     for (int i = 0; i < this.size; i++) {
-      if (this.items[i].equals(new GroceryItem(name, 0))) {
+      if (item.equals(items[i])) {
         return i;
       }
     }
@@ -63,18 +68,131 @@ class GroceryListArray implements IGroceryList {
     return -1;
   }
   
-  
   public boolean remove(String name) {
+    if (size == 0) {
+      return false; 
+    }
+    
+    GroceryItem item = new GroceryItem(name, 0);
+    for (int i = 0; i < this.size; i++) {
+      if (item.equals(items[i])) {
+        if(i == this.size - 1){
+          this.size--;
+        return true;
+        }else{// if(i == 0){
+          //GroceryItem temp = new GroceryItem(name, 0);
+          for(int j = indexOf(item); j < this.size - 1; j++){
+            items[j] = items[j + 1];
+          }
+          this.size--;
+        return true;
+        }
+          
+      }
+    
+    
+    }
     return false;
   }
   
   public boolean markAsBought(String name) {
+    GroceryItem item = new GroceryItem(name, 0);
+    for (int i = 0; i < this.size; i++) {
+      if (item.equals(items[i])) {
+        items[i].buy();
+        return true;
+      }
+    }
+    
     return false;
   }
   
+
   public void display() {
     for (int i = 0; i < this.size; i++) {
       System.out.println(items[i]);
     }
+  }
+  
+  // -> int
+  //Returns the number of individiual items in this GroceryList
+  /* Fields:
+       * max -- int
+       * items[] -- GroceryItem
+       * size -- int
+       * 
+       * Methods:
+       * indexOf(GroceryItem)
+       * remove(String)
+       * markAsBought(String)
+       * 
+       * Methods of GroceryItem:
+       * addQuantity(int)
+       * buy()
+       * toString()
+       * equals(Object)
+       * 
+       * -> int
+       * //returns the quantity of this GroceryItem
+       * for(int i = 0; i < this.size; i++){
+       * 
+       * }
+       * 
+       *int getQuantity() {
+       *   return 0;
+       * }
+       * 
+       */
+  public int totalQuantity() {
+    int sum = 0;
+    for(int i = 0; i < this.size; i++){
+      sum = sum + items[i].getQuantity();
+    }
+    return sum;
+  }
+  
+  //String, int -> boolean
+  //Returns true if the quantity of a given item has been reduced.
+  //The item is removed if it's quantity is zero or less
+
+  /* Fields:
+       * max -- int
+       * items[] -- GroceryItem
+       * size -- int
+       * 
+       * Methods:
+       * indexOf(GroceryItem)
+       * remove(String)
+       * markAsBought(String)
+       * 
+       * 
+       * Methods of GroceryItem:
+       * addQuantity(int)
+       * buy()
+       * toString()
+       * equals(Object)
+       * getQuantity()
+       * 
+       * for(int i = 0; i < this.size; i++){
+       * 
+       * }
+       * 
+       * int -> void
+       * reduces the quantity of this GroceryItem
+       * void reduce(int) {
+       * }
+       */
+  public boolean reduceQuantity(String itemName, int quantity){
+    GroceryItem item = new GroceryItem(itemName, 0);
+    for (int i = 0; i < this.size; i++) {
+      if (item.equals(items[i])) {
+        items[i].reduce(quantity);
+        if(items[i].getQuantity() < 1){
+          remove(itemName);
+        }
+        return true;
+      }
+    }
+    return false;
   }
 }
